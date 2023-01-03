0b1eb7f523f5a1a11316f8905b6877afa6f9aa1a
akochariyeva <akochariyeva12137@ada.edu.az>
2023-01-03 20:39:41 +0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Container, Box } from "@mui/material";
+import Navbar from "./Navbar";
+import HomePage from "./HomePage";
+import MoviesPage from "./Movies";
+import ContactPage from "./ContactPage";
 
 function App() {
   return (
     <div className="App">
- 
+      <Router>
+        <Container maxWidth="md">
+          <Box sx={{ bgcolor: "white", height: "100vh" }}>
+            <Navbar />
+            <Routes>
+              <Route exact element={<HomePage />} path="/" />
+              <Route element={<MoviesPage />} path="/movies" />
+              <Route element={<ContactPage />} path="/contact" />
+            </Routes>
+          </Box>
+        </Container>
+      </Router>
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,22 @@
+import { Box, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import { NavLink } from "react-router-dom";
+const Navbar = () => {
+  const navItems = ["Home", "Movies", "Contact"];
+  return (
+    <Box sx={{ textAlign: "center" }}>
+      <List sx={{ display: "flex", justifyContent: "space-evenly" }}>
+        {navItems.map((item) => (
+          <NavLink to={item === "Home" ? "/" : item.toLowerCase()} style={({ isActive }) => (isActive ? { textDecoration: "underline" } : undefined)}>
+            <ListItem key={item} disablePadding>
+              <ListItemButton sx={{ textAlign: "center" }}>
+                <ListItemText primary={item} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
+        ))}
+      </List>
+    </Box>
+  );
+};
+
+export default Navbar;
