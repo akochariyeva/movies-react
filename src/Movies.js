7668a4e941b7d1e6cfe471474cd84c8a03c21e51
crypteex <mehdi_2002@list.ru>
2023-01-04 03:20:23 +0400

    change

--- src/Movies.js
+++ src/Movies.js
@@ -14,9 +14,15 @@ const Movies = () => {
   return (
     <Box>
       <Typography variant="h3" component="h2" marginBottom={5}>
-        Movies
+        All Movies
       </Typography>
-      <Grid container spacing={2} direction="row" justify="flex-start" alignItems="flex-start">
+      <Grid
+        container
+        spacing={2}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
         {movies.map((movie) => (
           <Grid item xs={12} key={movie.id}>
             <MovieCard movie={movie} />
